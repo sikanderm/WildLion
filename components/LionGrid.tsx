fb38f3ab3b47bbc791cfd1817d7cca626e3bbdc6
sikandermohammad@ymail.com <61781379+sm1853@users.noreply.github.com>
2025-07-15 02:33:47 -0400

    Adding new sitemap

--- components/LionGrid.tsx
+++ components/LionGrid.tsx
@@ -21,7 +21,13 @@ interface LionGridProps {
 }
 
 const getRandomItems = (arr: Lion[], count = 6): Lion[] => {
-  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+  const shuffled = [...arr];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
   return shuffled.slice(0, count);
 };
 
